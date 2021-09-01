093b7652dd818858f0e87dc9c4aa91e2eef50c85
jomas94 <jomariojsa@gmail.com>
2021-09-01 11:57:07 +0200

    directiva personalizada

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -6,11 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicosComponent implements OnInit {
 
+export class BasicosComponent implements OnInit {
+  
+  @ViewChild('miFormulario') miFormulario!: NgForm;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  nombreValido():boolean{
+    return this.miFormulario?.controls.producto?.invalid
+            &&  this.miFormulario?.controls.producto?.touched
+  }
+
+  precioValido():boolean{
+    return this.miFormulario?.controls.precio?.value < 0
+            &&  this.miFormulario?.controls.producto?.touched;
+  }
+
+  guardar(){
+    console.log(this.miFormulario);
+    
+    
   }
 
 }
