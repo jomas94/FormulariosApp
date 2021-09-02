4b5f09366e6138f06419112b5107378dc979ed99
jomas94 <jomariojsa@gmail.com>
2021-09-02 11:38:18 +0200

    init and reset form

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -9,27 +9,38 @@ import { NgForm } from '@angular/forms';
 })
 
 export class BasicosComponent implements OnInit {
-  
   @ViewChild('miFormulario') miFormulario!: NgForm;
+  
+  initForm = {
+    producto: 'Holaquetal',
+    precio: 10,
+    existencias: 10
+  }
   constructor() { }
-
+  
   ngOnInit(): void {
   }
   
   nombreValido():boolean{
     return this.miFormulario?.controls.producto?.invalid
-            &&  this.miFormulario?.controls.producto?.touched
+    &&  this.miFormulario?.controls.producto?.touched
   }
-
+  
   precioValido():boolean{
     return this.miFormulario?.controls.precio?.value < 0
-            &&  this.miFormulario?.controls.producto?.touched;
+    &&  this.miFormulario?.controls.producto?.touched;
+    
   }
 
   guardar(){
     console.log(this.miFormulario);
     
-    
+    this.miFormulario.resetForm({
+      producto: 'nuevonombre',
+      precio: 33,
+      existencias: 12+'13'
+    })
+
   }
 
 }
