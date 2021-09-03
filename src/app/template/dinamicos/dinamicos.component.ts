6fe7bab12fe5efb5837850a8318561767d19e0c0
jomas94 <jomariojsa@gmail.com>
2021-09-03 23:27:23 +0200

    eleminar juego function

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+
+interface Persona{
+  nombre: string;
+  favoritos: Favorito[],
+}
+
+interface Favorito{
+  id: number,
+  favorito: string
+}
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +18,33 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
 
-  constructor() { }
+export class DinamicosComponent {
 
-  ngOnInit(): void {
+  @ViewChild('miFormularioDinamico') miFormularioDinamico!: NgForm;
+
+  persona: Persona = {
+    nombre: 'Jomario',
+    favoritos: [
+      { id: 1, favorito: 'MarioKart'},
+      { id: 2, favorito: 'MortalKombat'},
+    ]
   }
+
+  guardar(){
+    console.log('formulario');
+    
+  }
+
+  nombreValido():boolean{
+    return this.miFormularioDinamico?.controls.producto?.errors ? true: false;
+  }
+
+  eliminar( i:number){
+    this.persona.favoritos.splice(i, 1);
+  }
+
+
+
 
 }
