b797c7cffd0e2bac0ad5305fcfd6f6647edfc53b
jomas94 <jomariojsa@gmail.com>
2021-09-07 23:37:46 +0200

    reactive form dinamico

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +7,55 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  miForm: FormGroup = this.formBuilder.group({
+    nombre: ['' , [Validators.required, Validators.minLength(2), ]],
+    favoritos: this.formBuilder.array([
+      ['Samsung',],
+      ['Xaomi',]
+    ], Validators.required)
+  })
 
-  ngOnInit(): void {
+  nuevoFavorito:FormControl = this.formBuilder.control('', Validators.required);
+
+
+
+  get favoritoArr(){
+    return this.miForm.get('favoritos') as FormArray;
   }
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  campoNoEsValido( input: string){
+    return this.miForm.controls[input].errors 
+          && this.miForm.controls[input].touched;
+  }
+
+  guardar(){
+    
+    if(this.miForm.invalid){
+      this.miForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.miForm.value);
+  }
+
+  agregarFavorito(){
+     if(this.nuevoFavorito.invalid){
+        return;
+    }
+    // this.favoritoArr.push( new FormControl(this.nuevoFavorito.value, Validators.required)); 
+    this.favoritoArr.push(this.formBuilder.control(this.nuevoFavorito.value, Validators.required)); 
+    this.nuevoFavorito.reset();
+  }
+
+  borrar(fav:number){
+    this.favoritoArr.removeAt(fav);
+    console.log('borrar');
+    
+  }
+
 
 }
